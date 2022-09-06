e79fe33f465ac00ec44f6d8331f2b55411be4f2d
Doug A <douganderson444@gmail.com>
2022-09-06 14:46:49 -0300

    add vite polyfills

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,36 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import path, { dirname } from 'path';
+import * as buffer from 'buffer';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
+	server: {
+		fs: {
+			strict: false
+		}
+	},
+	define: {
+		'process.env': {}
+		// Buffer: buffer.Buffer
+	},
 	resolve: {
 		alias: {
-			'@peerpiper/iframe-wallet-sdk': path.resolve('src/lib')
-		}
+			'@peerpiper/iframe-wallet-sdk': path.resolve('src/lib'),
+			process: 'process/browser',
+			util: 'util',
+			stream: 'stream-browserify',
+			assert: 'assert',
+			crypto: 'crypto-browserify',
+			Buffer: buffer.Buffer
+		},
+		mainFields: ['browser', 'module', 'main']
 	},
 	// workaround for common-js issues of default export
 	// https://vitejs.dev/guide/dep-pre-bundling.html#monorepos-and-linked-dependencies
 	build: {
 		commonjsOptions: {
-			include: [/immortal-db/, /node_modules/]
+			include: [/immortal-db/, /node_modules/, /arbundles/]
 		},
 		minify: false,
 		sourcemap: true,
@@ -25,7 +42,11 @@ const config = {
 		minimize: false
 	},
 	optimizeDeps: {
-		include: ['immortal-db']
+		include: [
+			'immortal-db'
+			// , 'arbundles'
+		]
+		// exclude: ['arbundles']
 	}
 };
 
