8b90c82d6ec6a060d61bd30c302957b97a5846cb
Doug A <douganderson444@gmail.com>
2022-09-06 19:39:33 -0300

    add Buffer, process, events, and global to window

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,27 @@
 <script>
 	import '../app.css';
+	import { onMount } from 'svelte';
+	import * as EventEmitter from 'events';
+	import * as process from 'process';
+	import { Buffer } from 'buffer';
+
+	let mounted;
+	onMount(async () => {
+		// import buffer
+		// const { default: buffer } = await import('buffer');
+		// window.Buffer = buffer.Buffer;
+
+		window.Buffer = Buffer;
+		window.process = process;
+		window.EventEmitter = EventEmitter;
+		window.global = window;
+
+		mounted = true;
+	});
 </script>
 
 <div class="bg-neutral-700 relative flex flex-col h-screen">
-	<slot />
+	{#if mounted}
+		<slot />
+	{/if}
 </div>
