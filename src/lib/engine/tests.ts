653e291ebede00a90be150e2a0715a5bc2a33cb3
Doug A <douganderson444@gmail.com>
2022-09-06 14:46:36 -0300

    add Bundlr dispatch test

---
+++ src/lib/engine/tests.ts
@@ -0,0 +1,36 @@
+// test dispatch in browser
+// import dispatch from arweave handler
+import { handlers as wallet, generateRsaJwk, getLoadedKeys } from '@peerpiper/iframe-wallet-sdk';
+
+import Arweave from 'arweave';
+import { JWKInterface } from 'arweave/node/lib/wallet';
+
+let arweave: Arweave;
+
+async function bundle(arweave) {
+	const transaction = await arweave.createTransaction({
+		data: '<html><head><meta charset="UTF-8"><title>Hello Bundlr!</title></head><body>Hello Bundlr!</body></html>'
+	});
+
+	transaction.addTag('Content-Type', 'text/html');
+	transaction.addTag('test', 'test');
+
+	console.log({ transaction });
+
+	const dataEntry = await wallet.arweaveWalletAPI.dispatch(transaction);
+	console.log({ dataEntry });
+
+	return dataEntry;
+}
+
+export async function flow() {
+	await generateRsaJwk();
+	const keys = getLoadedKeys();
+
+	arweave = Arweave.init({});
+	const Tx = await bundle(arweave);
+
+	console.log(`IDs: ${JSON.stringify(Tx)}`);
+
+	return Tx;
+}
---
+++ src/routes/dispatch.svelte
@@ -0,0 +1,25 @@
+<script>
+	// @ts-nocheck
+
+	// test dispatch in browser
+	import { onMount } from 'svelte';
+	let result;
+	onMount(async () => {
+		// import buffer
+		const { default: buffer } = await import('buffer');
+		window.Buffer = buffer.Buffer;
+
+		// set window.EventEmitter from events
+		const { EventEmitter } = await import('events');
+		window.EventEmitter = EventEmitter;
+
+		window.global = window;
+
+		const { flow } = await import('../lib/engine/tests');
+		result = await flow();
+	});
+</script>
+
+{#if result}
+	{JSON.stringify(result)}
+{/if}
